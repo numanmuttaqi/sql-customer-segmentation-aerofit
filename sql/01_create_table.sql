1102b7389009a14f8c879b6225cef1165e9ece03
numanmuttaqi <numan.muttaqi@tum.de>
2026-06-10 19:34:43 +0200

    Create new documents, text description, create database

---
+++ sql/01_create_table.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS aerofit_customers;
+
+CREATE TABLE aerofit_customers (
+    product VARCHAR(10),
+    age INT,
+    gender VARCHAR(10),
+    education INT,
+    marital_status VARCHAR(20),
+    usages INT,
+    fitness INT, 
+    income INT, 
+    miles INT 
+);
+
